90956b2cadca5f14470e7f3e2697f24f4fd203bd
Tomasz Sterna <tomasz@sterna.link>
2026-01-01 00:15:34 +0100

    Standardize tSU on types defines

--- src/chips/su/su.h
+++ src/chips/su/su.h
@@ -1,5 +1,6 @@
 /* su.cpp/su.h - Sound Unit emulator
  * Copyright (C) 2015-2023 tildearrow
+ * Copyright (C) 2025 smokku
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -19,88 +20,88 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  * SOFTWARE.
  */
-#include <stdio.h>
-#include <stdlib.h>
-#include <math.h>
+#include <stddef.h>
+#include <stdint.h>
 
-class SoundUnit {
-  signed char SCsine[256];
-  signed char SCtriangle[256];
-  signed char SCpantabL[256];
-  signed char SCpantabR[256];
-  unsigned int ocycle[8];
-  unsigned int cycle[8];
-  int rcycle[8];
-  unsigned int lfsr[8];
-  signed char ns[8];
-  short fns[8];
-  short nsL[8];
-  short nsR[8];
-  short nslow[8];
-  short nshigh[8];
-  short nsband[8];
-  int tnsL, tnsR;
-  unsigned char ilBufPeriod;
-  unsigned short ilBufPos;
-  signed char ilFeedback0;
-  signed char ilFeedback1;
-  unsigned short oldfreq[8];
-  unsigned int pcmSize;
-  bool dsOut;
-  unsigned char dsChannel;
+class SoundUnit
+{
+    int8_t SCsine[256];
+    int8_t SCtriangle[256];
+    int8_t SCpantabL[256];
+    int8_t SCpantabR[256];
+    uint32_t ocycle[8];
+    uint32_t cycle[8];
+    int32_t rcycle[8];
+    uint32_t lfsr[8];
+    int8_t ns[8];
+    int16_t fns[8];
+    int16_t nsL[8];
+    int16_t nsR[8];
+    int16_t nslow[8];
+    int16_t nshigh[8];
+    int16_t nsband[8];
+    int32_t tnsL, tnsR;
+    uint8_t ilBufPeriod;
+    uint16_t ilBufPos;
+    int8_t ilFeedback0;
+    int8_t ilFeedback1;
+    uint16_t oldfreq[8];
+    uint32_t pcmSize;
+    bool dsOut;
+    uint8_t dsChannel;
+
   public:
-    unsigned short resetfreq[8];
-    unsigned short voldcycles[8];
-    unsigned short volicycles[8];
-    unsigned short fscycles[8];
-    unsigned char sweep[8];
-    unsigned short swvolt[8];
-    unsigned short swfreqt[8];
-    unsigned short swcutt[8];
-    unsigned short pcmdec[8];
+    uint16_t resetfreq[8];
+    uint16_t voldcycles[8];
+    uint16_t volicycles[8];
+    uint16_t fscycles[8];
+    uint8_t sweep[8];
+    uint16_t swvolt[8];
+    uint16_t swfreqt[8];
+    uint16_t swcutt[8];
+    uint16_t pcmdec[8];
     struct SUChannel {
-      unsigned short freq;
-      signed char vol;
-      signed char pan;
-      unsigned char flags0;
-      unsigned char flags1;
-      unsigned short cutoff;
-      unsigned char duty;
-      unsigned char reson;
-      unsigned short pcmpos;
-      unsigned short pcmbnd;
-      unsigned short pcmrst;
-      struct {
-        unsigned short speed;
-        unsigned char amt;
-        unsigned char bound;
-      } swfreq;
-      struct {
-        unsigned short speed;
-        unsigned char amt;
-        unsigned char bound;
-      } swvol;
-      struct {
-        unsigned short speed;
-        unsigned char amt;
-        unsigned char bound;
-      } swcut;
-      unsigned char special1C;
-      unsigned char special1D;
-      unsigned short restimer;
+        uint16_t freq;
+        int8_t vol;
+        int8_t pan;
+        uint8_t flags0;
+        uint8_t flags1;
+        uint16_t cutoff;
+        uint8_t duty;
+        uint8_t reson;
+        uint16_t pcmpos;
+        uint16_t pcmbnd;
+        uint16_t pcmrst;
+        struct {
+            uint16_t speed;
+            uint8_t amt;
+            uint8_t bound;
+        } swfreq;
+        struct {
+            uint16_t speed;
+            uint8_t amt;
+            uint8_t bound;
+        } swvol;
+        struct {
+            uint16_t speed;
+            uint8_t amt;
+            uint8_t bound;
+        } swcut;
+        uint8_t special1C;
+        uint8_t special1D;
+        uint16_t restimer;
     } chan[8];
-    signed char pcm[65536];
+    int8_t pcm[65536];
     bool muted[8];
-    void SetIL0(unsigned char addr);
-    void Write(unsigned char addr, unsigned char data);
-    void NextSample(short* l, short* r);
-    inline int GetSample(int ch) {
-      int ret=(nsL[ch]+nsR[ch])>>1;
-      if (ret<-32768) ret=-32768;
-      if (ret>32767) ret=32767;
-      return ret;
+    void Write(uint8_t addr, uint8_t data);
+    void NextSample(int16_t* l, int16_t* r);
+    inline int32_t GetSample(int32_t ch) {
+        int32_t ret = (nsL[ch] + nsR[ch]) >> 1;
+        if (ret < -32768) ret = -32768;
+        if (ret > 32767) ret = 32767;
+        return ret;
     }
-    void Init(int sampleMemSize=8192, bool dsOutMode=false);
+    void Init(size_t sampleMemSize = 8192, bool dsOutMode = false);
     void Reset();
     SoundUnit();
 };
